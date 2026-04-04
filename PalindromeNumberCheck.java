a9af22b59617062d4e7e64268342a9f5c78fbbae
harshali533 <harshalikamble.hli@gmail.com>
2026-04-04 23:11:01 +0530

    10th day

---
+++ PalindromeNumberCheck.java
@@ -0,0 +1,25 @@
+public class PalindromeNumberCheck
+{
+    public static void main(String[] args)
+    {
+        // variables initialization
+        int num = 12021, reverse = 0;
+
+        // palindrome if num and reverse are equal
+        if (getReverse(num, reverse) == num)
+            System.out.println(num + " is Palindrome");
+        else
+            System.out.println(num + " is not Palindrome");
+    }
+
+    static int getReverse(int num, int rev)
+    {
+        if (num == 0)
+            return rev;
+
+        int rem = num % 10;
+        rev = rev * 10 + rem;
+
+        return getReverse(num / 10, rev);
+    }
+}
